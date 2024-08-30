ffe3884b9ae0c4554d737fe0950a37fe575be7a1
Itohan <momoduitohan5@gmail.com>
2024-08-30 23:57:01 +0100

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,3 +19,28 @@ def filter_datum(fields: List[str],
 
     collect, replace = (patterns["collect"], patterns["replace"])
     return re.sub(collect(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+            Update the class to accept a list
+            of strings fields constructor argument
+        """
+
+        message = super(RedactingFormatter, self).format(record)
+        incoming = filter_datum(
+                self.fields, RedactingFormatter.REDACTION,
+                message, RedactingFormatter.SEPARATOR)
+        return incoming
