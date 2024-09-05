c7af9a07ba8d847e42777fd9a25cad03f4b02be5
Itohan <momoduitohan5@gmail.com>
2024-09-05 23:27:26 +0100

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -41,10 +41,10 @@ def session_login():
 
     if auth.require_auth(request.path, excluded_paths) is not True:
         user = auth.current_user(request)
-        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+        if auth.authorization_header(request) and auth.session_cookie(request):
             abort(401)
         if user is None:
-            abort(404)
+            abort(403)
         request.current_user = user
 
 
