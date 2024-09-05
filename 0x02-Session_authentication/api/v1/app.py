9ad2c1f0b01973e5fead0fb59f01cec3becc0752
Itohan <momoduitohan5@gmail.com>
2024-09-05 15:40:46 +0100

    Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -17,6 +17,9 @@ check = getenv("AUTH_TYPE")
 if check == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif check == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
