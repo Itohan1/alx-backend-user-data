4aa7f22006d5741f867d8b2d60dee04d00585ff5
Itohan <momoduitohan5@gmail.com>
2024-09-06 11:45:33 +0100

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,21 +33,17 @@ def session_login():
     """
 
     excluded_paths = [
-            'api/v1/status/'
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
             '/api/v1/auth_session/login/'
     ]
 
-    if auth and auth.require_auth(
-            request.path, excluded_paths) is False:
-        if auth.authorization_header(request) is None and \
-                auth.session_cookie(request) is None:
+    if auth and auth.require_auth(request.path, excluded_paths) is False:
+        if auth.authorization_header(request) and auth.session_cookie(request):
             abort(401)
-
         user = auth.current_user(request)
         if user is None:
-            abort(403)
+            abort(404)
         request.current_user = user
 
 
