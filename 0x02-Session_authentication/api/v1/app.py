4557ad7b9a924db68a9fec36bf3222675a3eb342
Itohan <momoduitohan5@gmail.com>
2024-09-07 14:04:29 +0100

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -41,7 +41,8 @@ def session_login():
     ]
 
     if auth and auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+        if auth.authorization_header(request) is None \
+                and auth.session_cookie(request) is None:
             abort(401)
         user = auth.current_user(request)
         if user is None:
