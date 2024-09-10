fd696f10112078510a605da9242b7c1609b0c8d2
Itohan <momoduitohan5@gmail.com>
2024-09-10 23:09:47 +0100

    Register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, make_response, request
+from auth import Auth
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -10,6 +11,20 @@ def check_form():
     """Flask app that has a single GET route"""
 
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def Register_user():
+    """User registeration"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 404
+
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
