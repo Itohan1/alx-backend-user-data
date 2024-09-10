275eec790d0051cd655aad6ac0ce814971608d66
Itohan <momoduitohan5@gmail.com>
2024-09-10 22:33:55 +0100

    Basic flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def check_form():
+    """Flask app that has a single GET route"""
+
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
