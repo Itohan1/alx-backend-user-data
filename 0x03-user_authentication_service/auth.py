1aa0ae395724a934f8574d3df2a4bee812d606fe
Itohan <momoduitohan5@gmail.com>
2024-09-11 11:15:10 +0100

    Generate uuids

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -18,7 +18,7 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """Generate uuids"""
 
     new_id = str(uuid.uuid4())
