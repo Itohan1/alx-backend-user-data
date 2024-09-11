b36fd29cdf96dcb25e22ea64cc27315a66b6c6a1
Itohan <momoduitohan5@gmail.com>
2024-09-11 15:34:15 +0100

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,8 +56,6 @@ def logout() -> str:
 
     session_id = request.cookies.get("session_id")
 
-    if not session_id:
-        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
