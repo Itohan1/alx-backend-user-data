46f3d1e621671d2d0460a39be2dcb8eff1672b20
Itohan <momoduitohan5@gmail.com>
2024-09-11 21:00:27 +0100

    User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,5 +75,17 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['GET'], strict_slashes=False)
+def reset_password():
+    """User profile"""
+    email = request.form.get('email')
+
+    try:
+        check_email = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": check_email})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
